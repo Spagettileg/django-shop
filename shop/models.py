25814c8cfffa2e68403738f9dc4b5dcf163e84db
Ubuntu <ubuntu@ip-172-31-38-41.eu-west-1.compute.internal>
2020-11-27 14:29:46 +0000

    New shop class model created that includes name, address and rating attributes.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField(blank=True)
+    rating = models.PositiveIntegerField(default=2, blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
