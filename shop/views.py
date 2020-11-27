1a1ffa64d066762456be24b12a7cb9ed6145bd60
Ubuntu <ubuntu@ip-172-31-38-41.eu-west-1.compute.internal>
2020-11-27 15:33:43 +0000

    View created and designed to link up to a queryset. Django Rest Framework mixins used to support build of an endpoint.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.mixins import CreateModelMixin
 
-# Create your views here.
+from .models import Shop
+from .serializers import ShopSerializer
+
+
+class ShopViewSet(CreateModelMixin): # POSTs
+
+      serializer_class = ShopSerializer
+      queryset = Shop.objects.all()
