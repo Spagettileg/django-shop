357a13c0320a8c6eae98127a63d70500bf70e650
Ubuntu <ubuntu@ip-172-31-38-41.eu-west-1.compute.internal>
2020-11-27 17:28:03 +0000

    router register required an updated due to be complinant with django v3.1.3 and latest django rest framework coding convention.

--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import include, re_path
 from rest_framework.routers import DefaultRouter
 from .views import ShopViewSet
 
-
 router = DefaultRouter()
-router.register(['shop'], ShopViewSet, base_name='shop')
+router.register(r'shop', ShopViewSet, basename='shop')
 
 urlpatterns = [
     re_path('^', include(router.urls)),
