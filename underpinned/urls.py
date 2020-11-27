373cfcd3afdbbbe57a33753a30212395f582b6c9
Ubuntu <ubuntu@ip-172-31-38-41.eu-west-1.compute.internal>
2020-11-27 16:08:46 +0000

    Urls file updated at the top table to ensure all apps are connected.

--- underpinned/urls.py
+++ underpinned/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from shop import urls as urls_shop
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^shop/', include(urls_shop)),
 ]
